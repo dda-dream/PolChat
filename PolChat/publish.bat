cac5e583c5fb3b0d2da3512fb120bb5b6dfb47db
d <d@BMAX-RYZEN7-874>
2026-04-27 09:13:33 +0300

    #

---
+++ PolChat/publish.bat
@@ -0,0 +1,1 @@
+dotnet publish PolChat.csproj -o bin\publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=false
