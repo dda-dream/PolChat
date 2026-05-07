b3246f71ee66e9d64a6ad139f2f4140504b6e766
user <user@NASDREAMPC>
2026-05-07 18:48:09 +0300

    #

--- PolChat/wwwroot/static/chat.ts
+++ PolChat/wwwroot/static/chat.ts
@@ -792,11 +792,6 @@ if (isChatPage) {
         }
     }
 
-    function getUserCountText(count: number): string {
-        if (count % 10 === 1 && count % 100 !== 11) return 'пользователь';
-        if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) return 'пользователя';
-        return 'пользователей';
-    }
 
     function updateMessageStatus(messageId: string, status: MessageStatusType) {
         // Обновляем кэш
