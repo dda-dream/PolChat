483362485a110dc6296f8998aad6652ace990bed
d <d@BMAX-W10>
2026-08-20 14:11:48 +0300

    #

--- PolChat/publish.bat
+++ PolChat/publish.bat
@@ -1,1 +1,3 @@
+set "PATH=C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Microsoft\VisualStudio\NodeJs;%PATH%"
+
 dotnet publish PolChat.csproj -o bin\publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=false
